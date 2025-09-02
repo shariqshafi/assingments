0eb8cbde3a94382c3ae6a7f2ec538be981edca66
Shariq Shafi <bhatshariq712@gmail.com>
2025-09-03 00:24:05 +0530

    table

--- task1/src/App.jsx
+++ task1/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+export default function App() {
+  const [users, setUsers] = useState([]);
 
-function App() {
-  const [count, setCount] = useState(0)
+useEffect(() => {
+  fetch("https://gorest.co.in/public/v2/users")
+    .then((res) => res.json())
+    .then((data) => setUsers(data))
+    .catch((err) => console.error("Error fetching users:", err));
+}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: 16 }}>
+      <h1>Users</h1>
+      <table border="1" cellPadding="8" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+       <tbody>
+  {users.map((u) => (
+    <tr key={u.id}>
+      <td>{u.id}</td>
+      <td>{u.name}</td>
+      <td>{u.email}</td>
+      <td>{u.gender}</td>
+      <td>{u.status}</td>
+    </tr>
+  ))}
+</tbody>
 
-export default App
+      </table>
+    </div>
+  );
+}
